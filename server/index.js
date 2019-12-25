7b75392c382ba48804c1d1bc1a171f32483b8c79
SheepTester <ussa.say@gmail.com>
2019-12-24 17:14:26 -0800

    Show BAUBLES

--- server/index.js
+++ server/index.js
@@ -10,12 +10,67 @@ const port = 3000
 const server = http.createServer()
 const wss = new WebSocket.Server({ server })
 
+const online = new Map() // ws -> userData
+
+let user = 0
+function toAll (data, but = null) {
+  for (const ws of online.keys()) {
+    if (ws !== but) {
+      ws.send(JSON.stringify(data))
+    }
+  }
+}
 wss.on('connection', ws => {
-  ws.on('message', msg => {
-    //
+  const id = ++user
+  const userData = {
+    username: `Individual ${++user}`,
+    colour: Math.random() * 0x1000000 | 0,
+    usernameSent: false,
+    x: 0,
+    y: 0,
+    id
+  }
+  online.set(ws, userData)
+
+  ws.on('message', data => {
+    const msg = JSON.parse(data)
+    if (msg.type === 'message') {
+      toAll({ type: 'message', text: msg.text, from: userData.username })
+    } else if (msg.type === 'username') {
+      userData.username = msg.name
+      if (userData.usernameSent) {
+        toAll({ type: 'username-change', id, username: userData.username })
+      } else {
+        userData.usernameSent = true
+
+        ws.send(JSON.stringify({ type: 'message', text: `Welcome ${userData.username}! (only you can see this message)`, from: 'Server' }))
+        ws.send(JSON.stringify({
+          type: 'intros',
+          users: [...online.values()].map(({ id, username, colour, x, y }) => [id, { username, colour, x, y }])
+        }))
+        toAll({ type: 'message', text: `${userData.username} joined!`, from: 'Server' }, ws)
+        toAll({ type: 'hi', id, userData: {
+          username: userData.username,
+          colour: userData.colour,
+          x: userData.x,
+          y: userData.y
+        } }, ws)
+      }
+    } else if (msg.type === 'position') {
+      userData.x = msg.x
+      userData.y = msg.y
+      toAll({
+        type: 'positions',
+        positions: [...online.values()].map(({ id, x, y }) => ({ id, x, y }))
+      })
+    }
   })
 
-  ws.send(JSON.stringify({ type: 'message', text: 'Hello!', from: 'Server' }))
+  ws.on('close', () => {
+    toAll({ type: 'message', text: `${userData.username} left. :(`, from: 'Server' }, ws)
+    toAll({ type: 'bye', id }, ws)
+    online.delete(ws)
+  })
 })
 
 server.listen(port, () => {
